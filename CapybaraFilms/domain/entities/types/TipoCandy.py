c5954a9b3f62a081480661e62eeaefb95cd2b892
wandaoriana <wandalanatta5bod@gmail.com>
2025-06-12 18:22:21 -0300

    Arreglo la clase TipoCandy.py

--- CapybaraFilms/domain/entities/types/TipoCandy.py
+++ CapybaraFilms/domain/entities/types/TipoCandy.py
@@ -3,9 +3,9 @@ from enum import Enum
 # Definimos el enum TipoCandy
 class TipoCandy(Enum):
 
-    CHICO = ("Combo Chico", 2000)
-    MEDIANO = ("Combo Mediano", 4000)
-    GRANDE = ("Combo Grande", 5000)
+    CHICO = ("Combo Chico", 5000)
+    MEDIANO = ("Combo Mediano", 8000)
+    GRANDE = ("Combo Grande", 10000)
 
     def __init__(self, nombre, precio):
         self.nombre = nombre
