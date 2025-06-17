ee99918c6273c889fb0c71aeb28c580d9394e6b4
wandaoriana <wandalanatta5bod@gmail.com>
2025-06-16 22:15:52 -0300

    Agregando comentarios a la clase TipoCandy.py

--- CapybaraFilms/domain/entities/types/TipoCandy.py
+++ CapybaraFilms/domain/entities/types/TipoCandy.py
@@ -1,18 +1,22 @@
-from enum import Enum
+from enum import Enum  # Importamos la clase base Enum para crear enumeraciones
 
-# Definimos el enum TipoCandy
+# Definimos el enum TipoCandy que representa los tipos de combos disponibles
 class TipoCandy(Enum):
 
+    # Definimos las constantes del enum, cada una con un nombre y un precio
     CHICO = ("Combo Chico", 5000)
     MEDIANO = ("Combo Mediano", 8000)
     GRANDE = ("Combo Grande", 10000)
 
+    # Constructor del enum. Se ejecuta para cada valor definido arriba.
     def __init__(self, nombre, precio):
-        self.nombre = nombre
-        self.precio = precio
+        self.nombre = nombre    # Asigna el nombre descriptivo del combo
+        self.precio = precio    # Asigna el precio correspondiente al combo
 
+    # Método para obtener el nombre del combo
     def get_nombre(self):
         return self.nombre
 
+    # Método para obtener el precio del combo
     def get_precio(self):
         return self.precio
