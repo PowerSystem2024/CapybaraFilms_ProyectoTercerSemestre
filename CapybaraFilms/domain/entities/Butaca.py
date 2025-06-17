89b1b289a77e43727c10698d98632b6a4a5e88b4
Mariana Aguilera <marian5174@gmail.com>
2025-06-16 21:24:38 -0300

    Agrego comentarios a las clases

--- CapybaraFilms/domain/entities/Butaca.py
+++ CapybaraFilms/domain/entities/Butaca.py
@@ -1,34 +1,37 @@
 class Butaca:
     def __init__(self,id_butaca,fila, columna,categoria,estado=False):
-        self.id_butaca = id_butaca
-        self.categoria = categoria
-        self.fila = fila
-        self.columna = columna
-        self.estado = estado
-        
+        self.id_butaca = id_butaca #identificador de la butaca
+        self.categoria = categoria #categoría de la butaca(ej: común o premium)
+        self.fila = fila #fila donde se encuentra esa butaca
+        self.columna = columna #columna donde se encunetra la buatac
+        self.estado = estado  #estado de ocupación, si está ocupada o libre
+
+    #método para cambiar el estado de la butaca
     def set_estado(self, estado):
         self.estado = estado
 
+    #Nos devuelve la categoría de la butaca
     def get_categoria(self):
         return self.categoria
-    
+
+    #devuelve true si está ocupada y false si está libre
     def is_estado(self):
         return self.estado
 
-    def get_ubicacion(self):
+    def get_ubicacion(self):#la ubicación de la butaca seleccionada
         return self.ubicacion
     
-    def set_categoria(self, categoria):
+    def set_categoria(self, categoria):#si es común o premium
         self.categoria = categoria
 
     def set_ubicacion(self, ubicacion):
         self.ubicacion = ubicacion
     
-    def get_fila(self):
+    def get_fila(self): #fila donde está ubicada la butaca
         return self.fila
     
-    def get_columna(self):
+    def get_columna(self): #columna donde está ubicada la butaca
         return self.columna
 
-    def __str__(self):
+    def __str__(self): # texto a imprimir de la butaca seleccionada por el cliente
         return f"Categoria: {self.categoria}, Ubicacion: {self.ubicacion}, Estado: {'Ocupada' if self.estado else 'Libre'}"
--- CapybaraFilms/domain/entities/types/TipoButaca.py
+++ CapybaraFilms/domain/entities/types/TipoButaca.py
@@ -1,17 +1,17 @@
 from enum import Enum
 
 class TipoButaca(Enum):
-    COMUN = ("comun", 4000)  # Tipo de butaca común con un precio de 2000
-    PREMIUM = ("premium", 6500)  # Tipo de butaca premium con un precio de 3500
+    COMUN = ("comun", 4000)  # Tipo de butaca común con un precio
+    PREMIUM = ("premium", 6500)  # Tipo de butaca premium con un precio más elevado
 
     def __init__(self, nombre, precio):
         self.nombre = nombre  # Nombre del tipo de butaca
         self.precio = precio  # Precio del tipo de butaca
 
     def get_nombre(self):
-        #Devuelve el nombre del tipo de butaca
+        # Devuelve el nombre del tipo de butaca
         return self.nombre
 
     def get_precio(self):
-        #Devuelve el precio del tipo de butaca
+        # Devuelve el precio del tipo de butaca
         return self.precio
